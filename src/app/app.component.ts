69a77c73f96a455d881a3d61ad57aa6adcc97612
Aarti <aarti@support.unlocktech.in>
2025-04-04 17:29:08 -0500

    commit loginform

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from "./auth/signup/signup.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [ SignupComponent],
 })
-export class AppComponent {
-  title = 'reactive-forms';
-}
+export class AppComponent {}
